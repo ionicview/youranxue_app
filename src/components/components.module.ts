9a7a5234ecc320b11b2334dff897b734cb45aee6
ionicview <ionicview@163.com>
2018-08-12 15:31:35 +0900

    Katex Font add OK

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
+// import { YouChoiceComponent } from './youtex/you-choice/you-choice.component';
+// import { YouChoiceOptionComponent } from './youtex/you-choice-option/you-choice-option.component';
+// import { YouChoiceOptionlistComponent } from './youtex/you-choice-optionlist/you-choice-optionlist.component';
+import { YouImgComponent } from './you-img/you-img';
 
 @NgModule({
 	declarations: [
     // YouChoiceComponent,
     // YouChoiceOptionComponent,
     // YouChoiceOptionlistComponent,
-    // YouImgComponent,
+    YouImgComponent,
     ],
 	imports: [],
 	exports: [
     // YouChoiceComponent,
     // YouChoiceOptionComponent,
     // YouChoiceOptionlistComponent,
-    // YouImgComponent,
+    YouImgComponent,
     ]
 })
 export class ComponentsModule {}
---
+++ src/components/you-img/you-img.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+/**
+ * Generated class for the YouImgComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'you-img',
+  templateUrl: 'you-img.html'
+})
+
+export class YouImgComponent implements OnInit {
+
+  text: string;
+
+
+    @Input() src: string;
+  
+  
+    constructor() {
+    }
+  
+    ngOnInit(): void {
+      if (!this.src.startsWith('http')) {
+  
+        this.src = environment.apiUrl.concat('/').concat(this.src);
+        console.log("SRC:" + this.src);
+      }
+    }
+
+}
